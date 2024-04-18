ebee6d8360a914bdc172fb74bc8afd22bf81e5dc
talpidaex <bayrakogzhn@gmail.com>
2024-04-18 22:07:33 +0300

    testing

--- main.js
+++ main.js
@@ -1,70 +1,86 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const usb = require('usb');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const usb = require("usb");
 
 let windows = [];
 
 const webusb = new usb.WebUSB({
-    allowAllDevices: true
+  allowAllDevices: true,
 });
 
 const showDevices = async () => {
-    const devices = await webusb.getDevices();
-    const text = devices.map(d => `${d.vendorId}\t${d.productId}\t${d.serialNumber || '<no serial>'}`);
-    text.unshift('VID\tPID\tSerial\n-------------------------------------');
+  const devices = await webusb.getDevices();
+  const text = devices.map(
+    (d) => `${d.vendorId}\t${d.productId}\t${d.serialNumber || "<no serial>"}`
+  );
+  text.unshift("VID\tPID\tSerial\n-------------------------------------");
 
-    windows.forEach(win => {
-        if (win) {
-            win.webContents.send('devices', text.join('\n'));
-        }
-    });
+  windows.forEach((win) => {
+    if (win) {
+      win.webContents.send("devices", text.join("\n"));
+    }
+  });
+};
+
+const getAllLegacyDevices = async () => {
+  const devices = usb.getDeviceList();
+  const webUSB = [];
+  for (const device of devices) {
+    webUSB.push(convertToLegacyToWebUSB(device));
+  }
+  debugger;
+};
+
+const convertToLegacyToWebUSB = async (device) => {
+  return await usb.WebUSBDevice.createInstance(device);
 };
 
 const createWindow = () => {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    });
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    // and load the index.html of the app.
-    win.loadFile('index.html');
+  // and load the index.html of the app.
+  win.loadFile("index.html");
 
-    // Open the DevTools.
-    // win.webContents.openDevTools()
+  // Open the DevTools.
+  // win.webContents.openDevTools()
 
-    windows.push(win);
-    showDevices();
+  windows.push(win);
+  //   showDevices();
+  getAllLegacyDevices();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    webusb.addEventListener('connect', showDevices);
-    webusb.addEventListener('disconnect', showDevices);
+  webusb.addEventListener("connect", showDevices);
+  webusb.addEventListener("disconnect", showDevices);
 
-    createWindow();
+  createWindow();
 
-    app.on('activate', () => {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-    webusb.removeEventListener('connect', showDevices);
-    webusb.removeEventListener('disconnect', showDevices);
+app.on("window-all-closed", () => {
+  webusb.removeEventListener("connect", showDevices);
+  webusb.removeEventListener("disconnect", showDevices);
 
-    app.quit();
+  app.quit();
 });
 
 // In this file you can include the rest of your app's specific main process
